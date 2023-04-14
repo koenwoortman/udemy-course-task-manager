63b2a78392e5e1aacdcad7a32e6b1e2a61de0d57
Koen Woortman <koensw@outlook.com>
2023-04-14 20:25:45 +0200

    Section 1 - Video 4

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\TaskCollection;
+use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
@@ -11,5 +12,10 @@ class TaskController extends Controller
     public function index(Request $request)
     {
         return new TaskCollection(Task::all());
+    }
+
+    public function show(Request $request, Task $task)
+    {
+        return new TaskResource($task);
     }
 }
