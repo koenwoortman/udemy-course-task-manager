7b6a12b1937c8bda46c647d59ec529ad302c38af
Koen Woortman <koensw@outlook.com>
2023-04-14 14:02:23 +0200

    Section 1 - Video 2

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index(Request $request)
+    {
+        return response()->json(Task::all());
+    }
+}
