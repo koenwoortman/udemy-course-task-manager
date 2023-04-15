f0746b28845a7bb0271e1db4ee34db230cf73e03
Koen Woortman <koensw@outlook.com>
2023-04-15 11:41:23 +0200

    Section 3 - Video 2

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (! Auth::attempt($validated)) {
+            return response()->json([
+                'message' => 'Login information invalid',
+            ], 401);
+        }
+
+        $user = User::where('email', $validated['email'])->first();
+
+        return response()->json([
+            'access_token' => $user->createToken('api_token')->plainTextToken,
+            'token_type' => 'Bearer',
+        ]);
+    }
+}
