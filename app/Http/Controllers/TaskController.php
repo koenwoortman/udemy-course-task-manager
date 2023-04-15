fcd8f3b1353c4addd17789bdf824b7ee68a11782
Koen Woortman <koensw@outlook.com>
2023-04-15 11:59:27 +0200

    Section 3 - Video 4

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,6 +8,7 @@ use App\Http\Resources\TaskCollection;
 use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class TaskController extends Controller
@@ -32,7 +33,7 @@ class TaskController extends Controller
     {
         $validated = $request->validated();
 
-        $task = Task::create($validated);
+        $task = Auth::user()->tasks()->create($validated);
 
         return new TaskResource($task);
     }
