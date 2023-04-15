430982d6be33df65920abca05e28b89288b1e986
Koen Woortman <koensw@outlook.com>
2023-04-15 11:52:31 +0200

    Section 3 - Video 3

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('tasks', TaskController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('tasks', TaskController::class);
+});
