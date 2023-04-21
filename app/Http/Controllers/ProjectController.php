1b89e7d98fcadf72d0649b88a94777d661d452b8
Koen Woortman <koensw@outlook.com>
2023-04-21 21:00:30 +0200

    Section 4 - Video 6

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -44,4 +44,11 @@ class ProjectController extends Controller
 
         return new ProjectResource($project);
     }
+
+    public function destroy(Request $request, Project $project)
+    {
+        $project->delete();
+
+        return response()->noContent();
+    }
 }
